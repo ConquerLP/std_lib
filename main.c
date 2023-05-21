27c12b66247cefdcad2a9eaffa473b1cd26e0890
Stefan <StefanpaulSchool@gmail.com>
2023-05-21 21:57:04 +0200

    fixed String_insertCharAt writing out of heap

--- main.c
+++ main.c
@@ -2,19 +2,11 @@
 
 int main(void) {
 
-	String* str = String_ctor("boo:and:foo");
+	String* str = String_ctor("Test");
 
-	Array* test = Array_ctor("String", 20);
-	test->arrayIF->fill(test, str);
-
-
-	String* str1 = String_join(test);
-
-	printf("\n%s\n", str1->objectIF->toString(str1));
+	str->stringIF->insertCharAt(str, ' ', 0);
 
 	delete(str);
-	delete(str1);
-	delete(test);
 
 	PRINT_DEBUG_MEMORY;
 
