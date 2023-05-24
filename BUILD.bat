4fdad6ab93e112af41a57a9ceae857f62728d6c7
Stefan <StefanpaulSchool@gmail.com>
2023-05-24 23:21:05 +0200

    added build script

---
+++ BUILD.bat
@@ -0,0 +1,29 @@
+rmdir /q /s .git
+rmdir /q /s .vs
+rmdir /q /s x64
+del *.gitattributes
+del *.gitignore
+del *.md
+del *.vcxproj
+del *.vcxproj.filters
+del *.vcxproj.user
+del *.sln
+
+gcc -std=c11 -c def.c
+gcc -std=c11 -c basic.c
+gcc -std=c11 -c object.c
+gcc -std=c11 -c std_lib_math.c
+gcc -std=c11 -c vector.c
+gcc -std=c11 -c list.c
+gcc -std=c11 -c string.c
+gcc -std=c11 -c array.c
+gcc -std=c11 -c filemanager.c
+gcc -std=c11 -c template.c
+gcc -std=c11 -c main.c
+gcc -std=c11 def.o basic.o object.o std_lib_math.o vector.o list.o string.o array.o filemanager.o template.o main.o -o prog.exe
+
+cd /d %~dp0
+cls
+"prog.exe"
+
+pause
